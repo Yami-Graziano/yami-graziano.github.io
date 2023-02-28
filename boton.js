ba8495a87f9ec6e327a3400f1299137be55c3687
Yami-Graziano <126527940+Yami-Graziano@users.noreply.github.com>
2023-02-27 21:08:38 -0300

    Add files via upload

---
+++ boton.js
@@ -0,0 +1,19 @@
+document.getElementById('Age').onclick = function () {
+    console.log("Visualiza los datos")
+    document.getElementById("demo").innerHTML = "25"
+}
+
+document.getElementById('Mail').onclick = function () {
+    console.log("Visualiza los datos")
+    document.getElementById("demo").innerHTML = "m.velazco@mail.com"
+}
+
+document.getElementById('Address').onclick = function () {
+    console.log("Visualiza los datos")
+    document.getElementById("demo").innerHTML = "Thames 1077 - CABA"
+}
+
+document.getElementById('Cel').onclick = function () {
+    console.log("Visualiza los datos")
+    document.getElementById("demo").innerHTML = "1158569293"
+}
